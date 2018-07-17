cb7e18bfa7622a3dfe2692348c7d02278305eb97
cryptofox3 <pghodgson558@gmail.com>
2018-07-17 13:52:27 -0500

    Added 'GetCharCount' in StringCompression class

--- LINQ-Project/LINQ-Project/Program.cs
+++ LINQ-Project/LINQ-Project/Program.cs
@@ -6,7 +6,7 @@ namespace LINQ_Project
     {
         static void Main(string[] args)
         {
-            RemoveDuplicates.DisplayNamesNoDuplicates();
+            StringCompression.Program();
         }
     }
 }
